8de105b78ca7567fba4962c0eb3588925f7e8d18
Adriaan Beiertz <adriaanbd@gmail.com>
2020-01-04 15:55:27 -0500

    adding ship component

---
+++ src/components/Ship.js
@@ -0,0 +1,29 @@
+class Ship {
+  constructor(length = 2, position = [3, 6]) {
+    this.length = length;
+    this.position = position;
+    this.lives = length;
+  }
+
+  navigate(fromID, toID) {
+    console.log(fromID, toID);
+    const diff = Math.abs(fromID - toID);
+    const prevPosition = this.position;
+    if (fromID > toID) {
+      this.position = prevPosition.map((pos) => pos - diff);
+    } else {
+      this.position = prevPosition.map((pos) => pos + diff);
+    }
+  }
+
+  hit() {
+    this.length -= 1;
+    return true;
+  }
+
+  isSunk() {
+    return this.lives < 1;
+  }
+}
+
+export default Ship;
