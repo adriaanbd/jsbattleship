30d8879ce5a82323901aaea23d1a852446a305e3
Adriaan Beiertz <adriaanbd@gmail.com>
2020-01-08 22:01:16 -0500

    drag ops in game and created two boards on screen

--- src/factories/Board.js
+++ src/factories/Board.js
@@ -1,11 +1,11 @@
-import Cells from '../components/Cells';
 import Ship from './Ship';
+import Cell from '../components/Cell';
 
 class Board {
   constructor(size = 100, positions = {}) {
     this.size = size;
-    this.grid = Cells(size);
-    this.cells = Array.from(this.grid.children);
+    this.grid = Cell.generateCells(size);
+    this.cells = Array.from(this.grid.childNodes);
     this.ships = [];
     this.positions = positions;
   }
@@ -26,22 +26,28 @@ class Board {
     }
   }
 
-  addShip(ship) {
+  addShip(ship, player = 'human') {
     const hasPosition = ship.position.length;
     if (!hasPosition) ship.setSail(this.positions);
     const shipPos = ship.position;
-    shipPos.forEach((id) => {
-      const cell = this.cells[id];
-      cell.classList.add('ship');
-      cell.draggable = true;
-      this.positions[id] = ship; // add to state
-    });
+    if (player === 'computer') {
+      shipPos.forEach((id) => {
+        this.positions[id] = ship;
+      });
+    } else {
+      shipPos.forEach((id) => {
+        const cell = this.cells[id];
+        cell.classList.add('ship');
+        cell.draggable = true;
+        this.positions[id] = ship; // add to state
+      });
+    }
   }
 
-  addShips() {
+  addShips(player) {
     if (this.ships.length === 0) this.createShips();
     this.ships.forEach((ship) => {
-      this.addShip(ship);
+      this.addShip(ship, player);
     });
   }
 
@@ -55,85 +61,10 @@ class Board {
     });
   }
 
-  dragStart(event) {
-    event.dataTransfer.setData('text/plain', event.target.id);
-  }
-
-  dragDrop(event) {
-    const { dataTransfer, target } = event;
-    const prevID = dataTransfer.getData('text');
-    const ship = this.positions[prevID];
-    if (!ship) {
-      return
-    }
-    const { position } = ship;
-    ship.navigate(prevID, target.id); // change ship.position
-    const isWithinValidRange = ship.position.every((id) => {
-      const inGrid = id < 100 && id >= 0;
-      const isShip = this.positions[id];
-      const myShip = this.positions[id] === ship;
-      if (inGrid && isShip) { return myShip; }
-      return inGrid && !isShip;
-    });
-    if (isWithinValidRange) {
-      this.removeShip(position);
-      this.addShip(ship);
-    } else {
-      ship.position = position;
-    }
-  }
-
-  dragOver(event) {
-    event.preventDefault();
-  }
-
-  dragEnter(event) {
-    event.preventDefault();
-  }
-
-  isValidDrag(dragType, target) {
-    const isShip = target.classList.contains('ship');
-    const isCell = target.classList.contains('cell');
-    if (dragType === 'dragstart') { return isShip; }
-    if (dragType === 'drop') { return !isShip && isCell; }
-    if (dragType === 'dragenter') { return isCell; }
-    if (dragType === 'dragover') { return isCell; }
-  }
-
-  dragHandler(event) {
-    const { type, target } = event;
-    if (this.isValidDrag(type, target)) {
-      switch (type) {
-        case 'dragstart':
-          this.dragStart(event);
-          break;
-        case 'drop':
-          this.dragDrop(event, this.positions);
-          break;
-        case 'dragenter':
-          this.dragEnter(event);
-          break;
-        case 'dragover':
-          this.dragOver(event);
-          break;
-        default:
-          break;
-      }
-    }
-  }
-
-  setListeners(root) {
-    root.addEventListener('dragstart', (event) => this.dragHandler(event));
-    root.addEventListener('dragenter', (event) => this.dragHandler(event));
-    root.addEventListener('dragover', (event) => this.dragHandler(event));
-    root.addEventListener('drop', (event) => this.dragHandler(event));
-  }
-
-  setUp(root, gridName) {
+  setUp(root, gridName, player) {
     root.appendChild(this.grid);
     this.grid.className = gridName;
-    this.addShips();
-    this.setListeners(root);
+    this.addShips(player);
   }
 }
 
--- src/game.js
+++ src/game.js
@@ -1,13 +1,85 @@
 import Board from './factories/Board';
 
 const Game = (size, parent) => {
-  const play = () => {
-    const leftBoard = new Board(size);
-    const rightBoard = new Board(size);
-    const content = document.querySelector(parent);
+  let turn = 0;
+  let boards;
 
-    leftBoard.setUp(content, 'battle-grid');
-    rightBoard.setUp(content, 'battle-grid');
+  const turnHelper = () => {
+    turn = turn === 0 ? 1 : 0;
+  };
+
+  const dragStart = (event) => {
+    event.dataTransfer.setData('text/plain', event.target.id);
+  };
+
+  const dragDrop = (event, board) => {
+    const { dataTransfer, target } = event;
+    const prevID = dataTransfer.getData('text');
+    const ship = board.positions[prevID];
+    if (!ship) {
+      return;
+    }
+    const { position } = ship;
+    ship.navigate(prevID, target.id); // change ship.position
+    const isWithinValidRange = ship.position.every((id) => {
+      const inGrid = id < 100 && id >= 0;
+      const isShip = board.positions[id];
+      const myShip = board.positions[id] === ship;
+      if (inGrid && isShip) { return myShip; }
+      return inGrid && !isShip;
+    });
+    if (isWithinValidRange) {
+      board.removeShip(position);
+      board.addShip(ship);
+    } else {
+      ship.position = position;
+    }
+  };
+
+  const dragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const dragEnter = (event) => {
+    event.preventDefault();
+  };
+
+  const isValidDrag = (dragType, target) => {
+    const isShip = target.classList.contains('ship');
+    const isCell = target.classList.contains('cell');
+    if (dragType === 'dragstart') { return isShip; }
+    if (dragType === 'drop') { return !isShip && isCell; }
+    if (dragType === 'dragenter') { return isCell; }
+    if (dragType === 'dragover') { return isCell; }
+  };
+
+  const dragHandler = (event) => {
+    const { type, target } = event;
+    if (isValidDrag(type, target)) {
+      if (type === 'dragstart') dragStart(event);
+      else if (type === 'drop') {
+        const board = boards[turn];
+        dragDrop(event, board);
+      } else if (type === 'dragenter') dragEnter(event);
+      else if (type === 'dragover') dragOver(event);
+    }
+  };
+
+  const setListeners = (root, boards) => {
+    root.addEventListener('dragstart', (event) => dragHandler(event, boards));
+    root.addEventListener('dragenter', (event) => dragHandler(event, boards));
+    root.addEventListener('dragover', (event) => dragHandler(event, boards));
+    root.addEventListener('drop', (event) => dragHandler(event, boards));
+  };
+
+  const play = () => {
+    boards = [new Board(size), new Board(size)];
+    const content = document.querySelector(parent);
+    boards[0].setUp(content, 'battle-grid', 'human');
+    boards[1].setUp(content, 'battle-grid', 'computer');
+
+    console.log(boards[0].positions, boards[1].positions);
+    setListeners(content, boards);
   };
 
   return {
