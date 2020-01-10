d0e717efa16ac6aa4d4fa553b28074d6f15f2ab6
Adriaan Beiertz <adriaanbd@gmail.com>
2020-01-09 21:50:36 -0500

    ship factory

--- src/factories/Ship.js
+++ src/factories/Ship.js
@@ -1,41 +1,37 @@
 import FirstMate from '../helpers/FirstMate';
 
-class Ship {
-  constructor(length = 2, position = []) {
-    this.length = length;
-    this.position = position;
-    this.lives = length;
-    this.mate = new FirstMate(length);
-  }
+const Ship = (length = 2, position = []) => {
+  let lives = length;
+  const mate = new FirstMate(length);
 
-  setSail(positions) {
-    const route = this.mate.routes(positions);
-    this.position = route;
-  }
+  const setSail = (positions) => {
+    const route = mate.routes(positions);
+    position = route;
+  };
 
-  navigate(fromID, toID) {
+  const navigate = (fromID, toID) => {
     const diff = Math.abs(fromID - toID);
-    const prevPosition = this.position;
+    const prevPosition = position;
     if (fromID > toID) {
-      this.position = prevPosition.map((pos) => pos - diff);
+      position = prevPosition.map((pos) => pos - diff);
     } else {
-      this.position = prevPosition.map((pos) => pos + diff);
+      position = prevPosition.map((pos) => pos + diff);
     }
-  }
+  };
 
-  isValidPos(pos) {
+  const isValidPos = (pos) => {
     const valid = pos < 100 && pos > 0;
     return valid;
-  }
+  };
 
-  hit() {
-    this.lives -= 1;
+  const hit = () => {
+    lives -= 1;
     return true;
-  }
+  };
 
-  isSunk() {
-    return this.lives === 0;
-  }
-}
+  const isSunk = () => {
+    return lives === 0;
+  };
+};
 
 export default Ship;
